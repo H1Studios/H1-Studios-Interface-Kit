303ccac701a2bdd33e78e6f395b54d9a0e2a6589
John Ashenden <jaashenden@gmail.com>
2013-10-23 15:49:53 -0400

    Big grid updates

--- application/cache/twig/07/74/4f4a7cabe6ffe72e0abea48eb8ab.php
+++ application/cache/twig/07/74/4f4a7cabe6ffe72e0abea48eb8ab.php
@@ -34,17 +34,65 @@ class __TwigTemplate_07744f4a7cabe6ffe72e0abea48eb8ab extends Twig_Template
     {
         // line 6
         echo "\t
+\t<div class=\"container container-fluid container-pod\">
+\t\t
+\t\t<div class=\"row\">
+\t\t\t
+\t\t\t<div class=\"col-mini-3\">
+\t\t\t\t
+\t\t\t\t<p style=\"text-align: justify;\">
+\t\t\t\t\t
+\t\t\t\t\tCollaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.
+\t\t\t\t\t
+\t\t\t\t</p>
+\t\t\t\t
+\t\t\t</div>
+\t\t\t
+\t\t\t<div class=\"col-mini-3\">
+\t\t\t\t
+\t\t\t\t<p style=\"text-align: justify;\">
+\t\t\t\t\t
+\t\t\t\t\tCollaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.
+\t\t\t\t\t
+\t\t\t\t</p>
+\t\t\t\t
+\t\t\t</div>
+\t\t\t
+\t\t\t<div class=\"col-mini-3\">
+\t\t\t\t
+\t\t\t\t<p style=\"text-align: justify;\">
+\t\t\t\t\t
+\t\t\t\t\tCollaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.
+\t\t\t\t\t
+\t\t\t\t</p>
+\t\t\t\t
+\t\t\t</div>
+\t\t\t
+\t\t\t<div class=\"col-mini-3\">
+\t\t\t\t
+\t\t\t\t<p style=\"text-align: justify;\">
+\t\t\t\t\t
+\t\t\t\t\tCollaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.
+\t\t\t\t\t
+\t\t\t\t</p>
+\t\t\t\t
+\t\t\t</div>
+\t\t\t
+\t\t</div>
+\t\t
+\t</div>
+\t
 \t<div class=\"container container-pod\">
 \t\t
 \t\t<div class=\"row\">
 \t\t\t
-\t\t\t<div class=\"col-md-3\">
+\t\t\t<div class=\"col-medium-3\">
 \t\t\t\t
 \t\t\t\tTest
 \t\t\t\t
 \t\t\t</div>
 \t\t\t
-\t\t\t<div class=\"col-md-9\">
+\t\t\t<div class=\"col-medium-9\">
 \t\t\t\t
 \t\t\t\tTest
 \t\t\t\t
--- views/pages/styleguide/grid.php
+++ views/pages/styleguide/grid.php
@@ -4,17 +4,65 @@
 
 {% block page %}
 	
+	<div class="container container-fluid container-pod">
+		
+		<div class="row">
+			
+			<div class="col-mini-3">
+				
+				<p style="text-align: justify;">
+					
+					Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.
+					
+				</p>
+				
+			</div>
+			
+			<div class="col-mini-3">
+				
+				<p style="text-align: justify;">
+					
+					Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.
+					
+				</p>
+				
+			</div>
+			
+			<div class="col-mini-3">
+				
+				<p style="text-align: justify;">
+					
+					Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.
+					
+				</p>
+				
+			</div>
+			
+			<div class="col-mini-3">
+				
+				<p style="text-align: justify;">
+					
+					Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.
+					
+				</p>
+				
+			</div>
+			
+		</div>
+		
+	</div>
+	
 	<div class="container container-pod">
 		
 		<div class="row">
 			
-			<div class="col-md-3">
+			<div class="col-medium-3">
 				
 				Test
 				
 			</div>
 			
-			<div class="col-md-9">
+			<div class="col-medium-9">
 				
 				Test
 				
